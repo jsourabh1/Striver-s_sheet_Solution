4734e7e5fcbed6d0a928f0436c8233263dfcba5d
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2021-09-02 23:10:57 +0530

    Update question5_consecutive_one's.py

--- Day7/question5_consecutive_one's.py
+++ Day7/question5_consecutive_one's.py
@@ -22,3 +22,46 @@ class Solution:
             
         return ans
                 
+
+# Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+# Output: 6
+# Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+# Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        
+        
+        start=end=ans=count=0
+        
+        while end<len(nums):
+            
+            if  nums[end]==1:
+                end+=1
+                ans=max(ans,end-start)
+                
+            else:
+                
+                if count<k:
+                    end+=1
+                    count+=1
+                    
+                    ans=max(ans,end-start)
+                else:
+                    
+                    while start<=end and count>=k:
+                        
+                        if nums[start]==0:
+                            count-=1
+                            
+                        start+=1
+                        
+        return ans
+                    
+        
