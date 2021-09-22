9ed700d80b33b81469f1d5a6147c637ddcaf5836
Saurabh Jain <jsourabh861@gmail.com>
2021-09-22 21:29:27 +0530

    Update question1_levelorder.py

--- Day-18_BinaryTree2/question1_levelorder.py
+++ Day-18_BinaryTree2/question1_levelorder.py
@@ -33,4 +33,52 @@ def findSpiral(root):
             n-=1
             
     return ans
+
+#level order traversal using recursion 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def height(root):
+    
+    if not root:
+        return 0
+    left=height(root.left)
+    right=height(root.right)
+    
+    return 1+max(left,right)
+def printlevel(root,level,ans):
+    
+    if not root:
+        return ans
+    
+    if level==1:
+        ans.append(root.val)
+        return ans 
+    printlevel(root.left,level-1,ans)
+    printlevel(root.right,level-1,ans)
+    
+    return ans
+    
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        
+        n=height(root)
+        ans=[]
+        
+        for i in range(1,n+1):
+            
+            ans.append(printlevel(root,i,[]))
+            
+        return ans[::-1]
+        
+        
+                    
+                
+            
+        
             
